ac9bc77e15df00b1605bf64244be2c00eb3f1131
ruchika-equitec <155627308+ruchika-equitec@users.noreply.github.com>
2024-01-15 20:29:42 +0530

    init

---
+++ BlazorApp1/Data/IStudentService.cs
@@ -0,0 +1,8 @@
+﻿using BlazorApp1.Models;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+public interface IStudentService
+{
+    Task<List<StudTable>> GetStudentsAsync();
+}
---
+++ BlazorApp1/Data/StudentService.cs
@@ -0,0 +1,22 @@
+﻿using BlazorApp1.Models;
+
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+public class StudentService : IStudentService
+{
+    private readonly Ruchi_studContext _dbContext;
+
+    public StudentService(Ruchi_studContext dbContext)
+    {
+        _dbContext = dbContext;
+    }
+
+
+    public async Task<List<StudTable>> GetStudentsAsync()
+    {
+        return await _dbContext.StudTable.ToListAsync();
+    }
+
+}
